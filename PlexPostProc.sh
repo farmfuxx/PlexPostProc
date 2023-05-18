b0f1299e4c9873d835d3d82f003f7ac7f241340d
farmfuxx <github@fuck.farm>
2023-05-18 11:43:52 -0700

    use only ffmpeg transcoder

--- PlexPostProc.sh
+++ PlexPostProc.sh
@@ -11,7 +11,7 @@
 #  Version: 2022.2.7 (forked by apassiou)
 #
 #  Pre-requisites:
-#     ffmpeg (recommended) with libx265 or handbrakecli
+#     ffmpeg (required) with libx265
 #
 #  Usage:
 #     'PlexPostProc.sh %1'
@@ -23,10 +23,10 @@
 #      the show it is about to transcode.
 #
 #      2. Uses the selected encoder to transcode the original, very
-#      large MPEG2 format file to a smaller, more manageable H.264 mkv file
+#      large MPEG2 format file to a smaller, more manageable x265 mkv file
 #      (which can be streamed to various devices more easily).
 #
-#      3. Copies the file back to the original .grab folder for final processing
+#      3. Copies the file back to the original location for final processing
 #
 #  Log:
 #     Single log is generated with timestamped transcodes.
@@ -37,17 +37,12 @@
 #******************************************************************************
 
 TMPFOLDER="/tmp"
-ENCODER="ffmpeg"  # Encoder to use:
-                  # "ffmpeg" for FFMPEG [DEFAULT]
-                  # "handbrake" for HandBrake
-                  # "nvtrans" for Plex Transcoder with NVENC support
 RES="720"         # Resolution to convert to:
                   # "480" = 480 Vertical Resolution
                   # "720" = 720 Vertical Resolution
                   # "1080" = 1080 Vertical Resolution
 
 
-#**************************FFMPEG SPECIFIC SETTINGS****************************
 AUDIO_CODEC="ac3" # From best to worst: libfdk_aac > libmp3lame/eac3/ac3 > aac. But libfdk_acc requires manual compilaton of ffmpeg. For OTA DVR standard acc should be enough.
 AUDIO_BITRATE=96
 VIDEO_CODEC="libx265" # Will need Ubuntu 18.04 LTS or later. Otherwise change to "libx264". On average libx265 should produce files half in size of libx264  without losing quality. It is more compute intensive, so transcoding will take longer.
@@ -100,14 +95,6 @@ if [ ! -z "$1" ]; then
    if [[ PPP_CHECK -eq 0 ]]; then
      printf "$LOG_STRING_1" | tee -a $LOGFILE
    fi
-   if [[ $ENCODER == "handbrake" ]]; then
-     LOG_STRING_2="You have selected HandBrake"
-         if [[ PPP_CHECK -eq 0 ]]; then
-       printf "$LOG_STRING_1" | tee -a $LOGFILE
-     fi
-     HandBrakeCLI -i "$FILENAME" -f mkv --aencoder copy -e qsv_h264 --x264-preset veryfast --x264-profile auto -q 16 --maxHeight $RES --decomb bob -o "$TEMPFILENAME"
-     check_errs $? "Failed to convert using Handbrake."
-   elif [[ $ENCODER == "ffmpeg" ]]; then
      LOG_STRING_2="Using FFMPEG"
      LOG_STRING_3=" [$FILESIZE -> "
      if [[ PPP_CHECK -eq 0 ]]; then
@@ -125,35 +112,6 @@ if [ ! -z "$1" ]; then
      seconds_taken="$(( $seconds - (minutes_taken * 60) ))"
      LOG_STRING_4="$(ls -lh $TEMPFILENAME | awk ' { print $5 }')] - [$minutes_taken min $seconds_taken sec]\n"
      check_errs $? "Failed to convert using FFMPEG."
-   elif [[ $ENCODER == "nvtrans" ]]; then
-     export FFMPEG_EXTERNAL_LIBS="$(find ~/Library/Application\ Support/Plex\ Media\ Server/Codecs/ -name "libmpeg2video_decoder.so" -printf "%h\n")/"
-     check_errs $? "Failed to convert using smart Plex Transcoder (NVENC). libmpeg2video_decoder.so not found."
-     export LD_LIBRARY_PATH="/usr/lib/plexmediaserver:/usr/lib/plexmediaserver/lib/"
-
-     # Grab some dimension and framerate info so we can set bitrates
-     HEIGHT="$(/usr/lib/plexmediaserver/Plex\ Transcoder -i "$FILENAME" 2>&1 | grep "Stream #0:0" | perl -lane 'print $1 if /, \d{3,}x(\d{3,})/')"
-     FPS="$(/usr/lib/plexmediaserver/Plex\ Transcoder -i "$FILENAME" 2>&1 | grep "Stream #0:0" | perl -lane 'print $1 if /, (\d+(.\d+)*) fps/')"
-
-     if [[ -z "${HEIGHT}" ]]; then
-             # Failed to get dimensions of source... try a dumb transcode.
-             /usr/lib/plexmediaserver/Plex\ Transcoder -y -hide_banner -hwaccel nvdec -i "$FILENAME" -s hd$RES -c:v h264_nvenc -preset veryfast -c:a copy "$TEMPFILENAME"
-             check_errs $? "Failed to convert using simple Plex Transcoder (NVENC)."
-           else
-             # Smart transcode based on source dimensions and fps
-       # Bitrate vlaues (Mb). Assuming 1080i30 (ATSC max) has same needs as 720p60
-       # Assuming 60 fps needs 2x bitrate than 30 fps
-       MULTIPLIER=$(echo - | perl -lane "if (${FPS} < 59) {print 1.0} else {print 2.0}")
-       ABR=$(echo - | perl -lane "if (${HEIGHT} < 720) {print 1*${MULTIPLIER}} elsif (${HEIGHT} < 1080) {print 2*${MULTIPLIER}} else {print 4*${MULTIPLIER}}")
-       MBR=$(echo - | perl -lane "print ${ABR} * 1.5")
-       BUF=$(echo - | perl -lane "print ${MBR} * 2.0")
-       /usr/lib/plexmediaserver/Plex\ Transcoder -y -hide_banner -hwaccel nvdec -i "$FILENAME" -c:v h264_nvenc -b:v "${ABR}M" -maxrate:v "${MBR}M" -profile:v high -bf:v 3 -bufsize:v "${BUF}M" -preset:v hq -forced-idr:v 1 -c:a copy "$TEMPFILENAME"
-             check_errs $? "Failed to convert using smart Plex Transcoder (NVENC)."
-          fi
-   else
-     echo "Oops, invalid ENCODER string.  Using Default [FFMpeg]." | tee -a $LOGFILE
-     ffmpeg -i "$FILENAME" -s hd$RES -c:v libx264 -preset veryfast -vf yadif -c:a copy "$TEMPFILENAME"
-     check_errs $? "Failed to convert using FFMPEG."
-   fi
 
    # ********************************************************"
    # Encode Done. Performing Cleanup
